4e6ebef45cf6b8850380deef9cf559c7d3d29856
Talha Zaheer Malik <talhazaheerdev@gmail.com>
2026-05-14 22:40:51 +0500

    Firebase Connected

---
+++ android/build.gradle.kts
@@ -0,0 +1,30 @@
+buildscript {
+    dependencies {
+        classpath("com.google.gms:google-services:4.3.15")
+    }
+}
+
+allprojects {
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+val newBuildDir = rootProject.layout.buildDirectory.dir("../../build").get()
+rootProject.layout.buildDirectory.set(newBuildDir)
+
+subprojects {
+    val newSubprojectBuildDir = newBuildDir.dir(project.name)
+    layout.buildDirectory.set(newSubprojectBuildDir)
+}
+
+subprojects {
+    evaluationDependsOn(":app")
+}
+
+tasks.register<Delete>("clean") {
+    delete(rootProject.layout.buildDirectory)
+}
+
+apply plugin: 'com.google.gms.google-services'
